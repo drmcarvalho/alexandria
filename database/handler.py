8e80e8982f3213f1e7ef6d9e74bf94b8e2c18193
drmcarvalho <furiano.k@gmail.com>
2023-07-02 22:44:16 -0300

    funcao execute e funcao create tables

--- database/handler.py
+++ database/handler.py
@@ -1,3 +1,30 @@
-def database():
-    pass
+import sqlite3
 
+
+def connection():
+    return sqlite3.connect("alexandria.db")
+
+
+def query(con, statement, params=None):
+    c = con.cursor()
+    try:
+        if params is not None:
+            c.execute(statement, params)
+        else:
+            c.execute(statement)
+        for row in c:
+            yield row
+    finally:
+        c.close()
+
+
+def execute(con, statement, params=None):
+    c = con.cursor()
+    try:
+        if params is not None:
+            c.execute(statement, params)
+        else:
+            c.execute(statement)
+        con.commit()
+    finally:
+        c.close()
--- database/init.py
+++ database/init.py
@@ -1,3 +1,5 @@
+from handler import connection, execute
+
 create_tables_stmt = [
     """CREATE TABLE IF NOT EXISTS file (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -7,14 +9,16 @@ create_tables_stmt = [
     """CREATE TABLE IF NOT EXISTS tag (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
         name TEXT NOT NULL
-    )""",
+    );""",
     """CREATE TABLE IF NOT EXISTS FileTag (
         id_file INTEGER NOT NULL, 
         id_tag INTEGER NOT NULL
-    )""",
+    );""",
 ]
+
+connection = connection()
 
 
 def create_tables():
     for stmt in create_tables_stmt:
-        pass
+        execute(connection, stmt)
